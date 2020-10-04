b672b7ba4cb1e86655c143f8b555db13c3d3ee75
Deive <deive.leal@gmail.com>
2020-10-04 19:04:18 -0300

    Adicionada visualização do tipo bar chart race

---
+++ Data Visualization/Bar_Chart_Race/Covid_Example/Bar_Chart_Race_Example.py
@@ -0,0 +1,22 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+from IPython import get_ipython
+
+# %%
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import bar_chart_race as brc
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# %%
+get_ipython().system('pip install bar_chart_race')
+
+
+# %%
+
+
